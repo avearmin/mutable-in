e81b45d45419b9589ee3226be6aacda4d06b5948
avearmin <arminradoncic1@gmail.com>
2023-11-07 23:21:44 -0500

    Add Init() and Close() methods
    Init() puts the terminal into raw mode, and Close() restores the terminal.
    This is needed because the terminal needs to record key presses without
    waiting for enter. This is the first step for mutable-in to taker the
    place of os.Stdin

--- mutablein.go
+++ mutablein.go
@@ -1,9 +1,14 @@
 package mutablein
 
-import "bytes"
+import (
+	"bytes"
+
+	"golang.org/x/term"
+)
 
 type mutableIn struct {
-	buffer *bytes.Buffer
+	buffer    *bytes.Buffer
+	isRunning bool
 }
 
 func NewMutableIn() *mutableIn {
@@ -12,12 +17,29 @@ func NewMutableIn() *mutableIn {
 	}
 }
 
-func (m mutableIn) Read(p []byte) (n int, err error) {
+func (m *mutableIn) Init() *term.State {
+	m.isRunning = true
+	oldState, err := term.MakeRaw(0)
+	if err != nil {
+		panic(err)
+	}
+	return oldState
+}
+
+func (m *mutableIn) Close(oldState *term.State) {
+	m.isRunning = false
+	err := term.Restore(0, oldState)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (m *mutableIn) Read(p []byte) (n int, err error) {
 	n, err = m.buffer.Read(p)
 	return n, err
 }
 
-func (m mutableIn) Write(p []byte) (n int, err error) {
+func (m *mutableIn) Write(p []byte) (n int, err error) {
 	n, err = m.buffer.Write(p)
 	return n, err
 }
