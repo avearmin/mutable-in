fd0c569b3e204f9ea27225be96136450d98a2ea3
avearmin <arminradoncic1@gmail.com>
2023-11-15 23:40:50 -0500

    Use locks and signals for efficient I/O operations
    Introduce a Mutex for synchronization and a Cond for waiting/signaling in
    the MutableIn structure, ensuring thread-safe read and write operations on
    the buffer. The Cond helps coordinate between the reader and writer,
    preventing the reader from attempting to read when the buffer is empty,
    thus avoiding premature EOF errors.
    
    Additionally, the Write and WriteByte methods now print the written
    content to simulate character echoing during the writing process.

--- mutablein.go
+++ mutablein.go
@@ -6,23 +6,30 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"sync"
 )
 
 type MutableIn struct {
 	buffer    *bytes.Buffer
 	isRunning bool
+	mu        *sync.Mutex
+	cond      *sync.Cond
 }
 
 func NewMutableIn() *MutableIn {
-	return &MutableIn{
+	muIn := MutableIn{
 		buffer: bytes.NewBuffer([]byte{}),
+		mu:     &sync.Mutex{},
 	}
+	muIn.cond = sync.NewCond(muIn.mu)
+	return &muIn
 }
 
 func (m *MutableIn) Init() {
 	m.isRunning = true
 	exec.Command("stty", "-f", "/dev/tty", "cbreak", "min", "1").Run()
 	exec.Command("stty", "-f", "/dev/tty", "-echo").Run()
+	go m.simulateInput()
 }
 
 func (m *MutableIn) Close() {
@@ -35,6 +42,14 @@ func (m *MutableIn) Read(p []byte) (n int, err error) {
 	if !m.isRunning {
 		panic(notInitError)
 	}
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	// To avoid an EOF, we unlock and wait until the buffer has something
+	if m.buffer.Len() == 0 {
+		m.cond.Wait()
+	}
+
 	n, err = m.buffer.Read(p)
 	return n, err
 }
@@ -43,17 +58,40 @@ func (m *MutableIn) Write(p []byte) (n int, err error) {
 	if !m.isRunning {
 		panic(notInitError)
 	}
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
 	n, err = m.buffer.Write(p)
+	m.cond.Signal() // We signal to waiting readers the buffer has something
 	return n, err
+}
+
+func (m *MutableIn) WriteByte(c byte) error {
+	if !m.isRunning {
+		panic(notInitError)
+	}
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	err := m.buffer.WriteByte(c)
+	fmt.Print(string(c))
+	m.cond.Signal() // We signal to waiting readers the buffer has something
+	return err
 }
 
 func (m *MutableIn) simulateInput() {
 	stdin := bufio.NewReader(os.Stdin)
-	key, err := stdin.ReadByte()
 	for {
+		if !m.isRunning {
+			return
+		}
+		key, err := stdin.ReadByte()
 		if err != nil {
 			panic(err)
 		}
-		fmt.Print(key)
+		err = m.WriteByte(key)
+		if err != nil {
+			panic(err)
+		}
 	}
 }
