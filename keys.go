bb85e7471455d0314bdbd24bb104bda5b4d0293d
avearmin <arminradoncic1@gmail.com>
2023-11-22 15:21:43 -0500

    Add backspace key functionality

--- keys.go
+++ keys.go
@@ -8,6 +8,7 @@ var (
 	RightArrow = [3]byte{0x1b, 0x5b, 0x43}
 	LeftArrow  = [3]byte{0x1b, 0x5b, 0x44}
 	Enter      = [3]byte{0x0a}
+	Backspace  = [3]byte{0x7f}
 )
 
 type event struct {
@@ -17,6 +18,7 @@ type event struct {
 func keyEvents() map[[3]byte]event {
 	return map[[3]byte]event{
 		Enter:      {callback: handleEnter},
+		Backspace:  {callback: handleBackspace},
 		RightArrow: {callback: handleRightArrow},
 		LeftArrow:  {callback: handleLeftArrow},
 		UpArrow:    {callback: handleUpDownArrows},
@@ -24,10 +26,32 @@ func keyEvents() map[[3]byte]event {
 	}
 }
 
+// handlers
 func handleEnter(m *MutableIn) {
 	m.cond.Signal() // We signal to waiting readers the buffer has something
-	m.cursor = 0
+	m.cursor = -1   // Don't know whats up here. If I set the cursor to 0, then backspace does not work properly. But -1 works perfectly. What am I missing? Is 0 the wrong way to think about reseting cursor pos?
 	m.Write([]byte{'\n'})
+}
+
+func handleBackspace(m *MutableIn) {
+	if m.cursor < 1 {
+		return
+	}
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	var newData []byte
+	if m.cursor >= m.buffer.Len() {
+		newData = processDataWhenBackspaceAtEnd(m)
+	} else {
+		newData = proccessDataWhenBackspaceAtMiddle(m)
+	}
+
+	m.buffer.Reset()
+	m.buffer.Write(newData)
+
+	m.cursor--
+	redrawTermLine(m)
 }
 
 func handleRightArrow(m *MutableIn) {
@@ -36,6 +60,7 @@ func handleRightArrow(m *MutableIn) {
 	}
 	m.cursor++
 	fmt.Print("\033[C")
+
 }
 
 func handleLeftArrow(m *MutableIn) {
@@ -48,4 +73,30 @@ func handleLeftArrow(m *MutableIn) {
 
 func handleUpDownArrows(m *MutableIn) {
 	// These keys should do nothing
+}
+
+// helpers
+func processDataWhenBackspaceAtEnd(m *MutableIn) []byte {
+	data := m.buffer.Bytes()
+	newData := data[:len(data)-1]
+	return newData
+}
+
+func proccessDataWhenBackspaceAtMiddle(m *MutableIn) []byte {
+	data := m.buffer.Bytes()
+	first := data[:m.cursor]
+	second := data[m.cursor+1:]
+
+	newData := append(first, second...)
+	return newData
+}
+
+func redrawTermLine(m *MutableIn) {
+	fmt.Print("\033[1G")
+	fmt.Print("\033[2K")
+	fmt.Print(m.buffer.String())
+	fmt.Print("\r")
+	for i := 0; i < m.cursor; i++ {
+		fmt.Print("\033[C")
+	}
 }
